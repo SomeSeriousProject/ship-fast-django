e4cc35a46a1a4e50265a4782c09f726f1d6dd7c4
hirak <hirak@email.com>
2024-08-20 15:07:34 +0530

    google sign in working but needs refactoring

--- app/account/urls.py
+++ app/account/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
-from .views import signin_with_google
+from .views import signin_with_google, signin_with_google_confirm
 
 app_name = "account"
 
 urlpatterns = [
     path("google/signin/", signin_with_google, name="signin_with_google"),
+    path(
+        "google/signin/confirm/",
+        signin_with_google_confirm,
+        name="signin_with_google_confirm",
+    ),
 ]
